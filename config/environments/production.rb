4802280389630b2d12a71db5ff92706024e2c98d
yalem54321 <yalem54321@gmail.com>
2020-06-25 02:06:22 +0100

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "artak_production"
 
   config.action_mailer.perform_caching = false
+    
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => 'yalem54321@gmail.com',
+  :password       => 'tminus54321',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
