970a34c59ffb977f7024ac731fb703ff781dc0ee
yalem54321 <yalem54321@gmail.com>
2020-06-25 05:02:04 +0100

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -74,7 +74,7 @@ config.action_mailer.smtp_settings = {
 :address              => "smtp.gmail.com",
 :port                 => 587,
 :user_name            => ENV["GMAIL_USERNAME"],
-:password             => ENV["GMAIL_PASSWORD"],
+:password             => ENV["quhxlvybjaldjsgl"],
 :authentication       => "plain",
 :enable_starttls_auto => true
 }
