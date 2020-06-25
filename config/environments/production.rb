36891c10cd74c12690c232f93af9a9610c91881d
yalem54321 <yalem54321@gmail.com>
2020-06-25 17:59:46 +0100

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-config.action_mailer.default_url_options = { :host => 'https://glacial-atoll-34403.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://vectoredu.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
