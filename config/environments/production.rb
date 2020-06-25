925b213d74646d773e3194dce7dd8a33a83f3bfd
yalem54321 <yalem54321@gmail.com>
2020-06-25 02:20:10 +0100

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :domain         => 'mail.google.com',
-  :port           => 587,
-  :user_name      => 'yalem54321@gmail.com',
-  :password       => 'tminus54321',
-  :authentication => :plain,
-  :enable_starttls_auto => true
+config.action_mailer.default_url_options = { :host => 'https://vectoredu.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => ENV["GMAIL_USERNAME"],
+:password             => ENV["GMAIL_PASSWORD"],
+:authentication       => "plain",
+:enable_starttls_auto => true
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
